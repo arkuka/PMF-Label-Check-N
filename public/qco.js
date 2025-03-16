64339a6c379d62613015d789861d7c0fe5acf9c0
arkuka <arkuka@gmail.com>
2025-03-17 02:44:15 +1100

    change the path of the server side app

--- public/qco.js
+++ public/qco.js
@@ -365,7 +365,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      await fetch("/index", {
+      await fetch("/api/index", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(submittedData),
