f5a3004ffc364d14297d48990d4b16086a531fd5
arkuka <40722785+arkuka@users.noreply.github.com>
2025-09-08 21:47:45 +1000

    remove the unused g_prompted

--- public/qco.js
+++ public/qco.js
@@ -14,7 +14,6 @@ let g_headers = [];
 let g_productNameList = [];  // List of product names
 let g_productNameLabel = "";
 let g_isSubmitEnabled = false;
-let g_prompted = false;
 let g_showModal = false;
 let g_scannedBarcode = "";
 let g_currentField = "";  // Tracks the currently input field
